32d9eeda656fe189ef278dc6e435081a0112f6fd
Matthew Sato <satomm@stanford.edu>
2024-07-13 00:43:28 +0000

    Add initial cyclone dds files

---
+++ consumer.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from cyclonedds.domain import DomainParticipant
+from cyclonedds.topic import Topic
+from cyclonedds.sub import DataReader
+from cyclonedds.util import duration
+from cyclonedds.idl import IdlStruct
+
+@dataclass
+class Message(IdlStruct):
+    text: str
+
+participant = DomainParticipant()
+topic = Topic(participant, Message, "Announcements")
+reader = DataReader(participant, topic)
+
+# If we don't receive a single announcement for five minutes we want the script to exit.
+for msg in reader.take_iter(timeout=duration(minutes=5)):
+    print(msg.text)
