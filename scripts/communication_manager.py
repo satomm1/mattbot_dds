77fd5ae8674a4fa8ed6e5f847c441417d9e11c3f
Matthew Sato <satomm@stanford.edu>
2024-08-26 22:52:36 +0000

    Add location listener to communication manager

--- scripts/communication_manager.py
+++ scripts/communication_manager.py
@@ -3,8 +3,9 @@ from rospy_message_converter import message_converter
 import tf
 import rospkg
 from mattbot_image_detection.msg import DetectedObject
-from mattbot_dds.msg import AgentSubscription, AgentPath
+from mattbot_dds.msg import AgentSubscription, AgentPath, AgentLocation
 from nav_msgs.msg import Path
+from geometry_msgs.msg import Pose
 
 from cyclonedds.domain import DomainParticipant
 from cyclonedds.topic import Topic
@@ -38,6 +39,24 @@ class DataMessage(IdlStruct):
     sending_agent: int
     timestamp: int
     data: str
+
+@dataclass
+class Location(IdlStruct):
+    """
+    Represents the location of an agent.
+
+    Attributes:
+        agent_id (int): The ID of the agent.
+        timestamp (int): The timestamp of the location message.
+        x (float): The x-coordinate of the agent.
+        y (float): The y-coordinate of the agent.
+        theta (float): The orientation of the agent.
+    """
+    agent_id: int
+    timestamp: int
+    x: float
+    y: float
+    theta: float
 
 class SelfDataListener(Listener):
 
@@ -97,6 +116,48 @@ class OtherDataListener(Listener):
                 # This was a message to the agent, we can safely ignore
                 continue
 
+class LocationListener(Listener):
+    """
+    Listener class that handles location data for agents.
+
+    Attributes:
+        my_id (int): The ID of the listener.
+        agent_ids (list): List of agent IDs.
+        location (tuple): Tuple to store agent location.
+
+    Methods:
+        on_data_available(reader): Callback method called when data is available.
+    """
+
+    def __init__(self, my_id, topic_id, location_publisher):
+        super().__init__()
+        self.my_id = my_id
+        self.topic_id = topic_id
+        self.location_publisher = location_publisher
+
+    def on_data_available(self, reader):
+        for sample in reader.read():
+
+            # Ignore messages from self
+            if sample.agent_id == int(self.my_id):
+                continue
+
+            agent_location = AgentLocation()
+            agent_location.agentID.data = sample.agent_id
+
+            agent_pose = Pose()
+            agent_pose.position.x = sample.x
+            agent_pose.position.y = sample.y
+
+            quaternion = tf.transformations.quaternion_from_euler(0, 0, sample.theta)
+            agent_pose.orientation.x = quaternion[0]
+            agent_pose.orientation.y = quaternion[1]
+            agent_pose.orientation.z = quaternion[2]
+            agent_pose.orientation.w = quaternion[3]
+
+            agent_location.pose = agent_pose
+            self.location_publisher.publish(agent_location)
+
 class CommManager:
 
     def __init__(self):
@@ -114,6 +175,13 @@ class CommManager:
             Policy.History.KeepLast(depth=1)
         )
 
+        self.best_effort_qos = Qos(
+            Policy.Reliability.BestEffort,
+            Policy.Durability.Volatile,
+            Policy.Deadline(duration(milliseconds=1000))
+            # Policy.History.KeepLast(depth=1)
+        )
+
         # Create a DomainParticipant, Subscriber, and Publisher
         self.participant = DomainParticipant()
         self.subscriber = Subscriber(self.participant)
@@ -126,9 +194,12 @@ class CommManager:
         self.other_agent_data_listeners = {}
         self.data_reader = DataReader(self.subscriber, self.data_topic, listener=self.data_listener, qos=self.reliable_qos)
         self.other_agent_data_readers = {}
+        self.other_agent_location_listeners = {}
+        self.other_agent_location_readers = {}
 
         self.object_publisher = rospy.Publisher('/object_from_agent', DetectedObject, queue_size=10)
         self.path_publisher = rospy.Publisher('/path_from_agent', AgentPath, queue_size=10)
+        self.location_publisher = rospy.Publisher('/agent_location', AgentLocation, queue_size=10)
 
         self.cone_subscriber = rospy.Subscriber('/new_cone_map', DetectedObject, self.cone_callback, queue_size=10)
         self.agent_subscriber = rospy.Subscriber('/agent_to_subscribe', AgentSubscription, self.agent_subscription_callback, queue_size=10)
@@ -157,20 +228,34 @@ class CommManager:
         for agent in agents:
             if agent not in self.agents_subscribed:
                 self.agents_subscribed.add(agent)
+
                 topic_name = 'DataTopic' + str(agent)
                 topic = Topic(self.participant, topic_name, DataMessage)
                 self.other_agent_data_listeners[agent] = OtherDataListener(self.my_id, agent, self.object_publisher, self.path_publisher)
                 self.other_agent_data_readers[agent] = DataReader(self.subscriber, topic, listener=self.other_agent_data_listeners[agent], qos=self.reliable_qos)
+
+                location_topic_name = 'LocationTopic' + str(agent)
+                location_topic = Topic(self.participant, location_topic_name, Location)
+                self.other_agent_location_listeners[agent] = LocationListener(self.my_id, agent, self.location_publisher)
+                self.other_agent_location_readers[agent] = DataReader(self.subscriber, location_topic, listener=self.other_agent_location_listeners[agent], qos=self.best_effort_qos)
+
                 print("Subscribed to agent " + str(agent))
 
         agents_to_remove = []
         for agent in self.agents_subscribed:
             if agent not in agents:
                 agents_to_remove.append(agent)
+                
                 self.other_agent_data_readers[agent] = None
                 self.other_agent_data_readers.pop(agent)
                 self.other_agent_data_listeners[agent] = None
                 self.other_agent_data_listeners.pop(agent)    
+
+                self.other_agent_location_readers[agent] = None
+                self.other_agent_location_readers.pop(agent)
+                self.other_agent_location_listeners[agent] = None
+                self.other_agent_location_listeners.pop(agent)
+
                 print("Unsubscribed from agent " + str(agent))
         for agent in agents_to_remove:
             self.agents_subscribed.remove(agent)
