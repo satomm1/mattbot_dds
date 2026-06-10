2c7b371d59b256b827f11d54987e9741d1372d6d
Matthew Sato <satomm@stanford.edu>
2026-06-10 18:43:34 +0000

    Increase HEARTBEAT_STARTUP_DELAY_S from 5.0 to 15.0 seconds for improved node synchronization

--- src/dds_utils/config.py
+++ src/dds_utils/config.py
@@ -16,7 +16,7 @@ INIT_RETRY_SLEEP_S = 1.0
 # Allow DDS discovery/matching before the first enter sample.
 INIT_DISCOVERY_GRACE_S = 3.0
 # Heartbeat nodes start after entry_exit has had time to join the swarm.
-HEARTBEAT_STARTUP_DELAY_S = 5.0
+HEARTBEAT_STARTUP_DELAY_S = 15.0
 
 POSITION_INIT_RECENT_THRESHOLD_S = 5.0
 
